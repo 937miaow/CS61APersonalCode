e09b37a78fb61706d87d11411fdf09c4be3d4d2b
6.02*10^23 <937miaow@users.noreply.github.com>
2024-10-15 16:27:04 +0800

    lab06 finish

--- lab06/lab06.py
+++ lab06/lab06.py
@@ -7,6 +7,10 @@ class Transaction:
     def changed(self):
         """Return whether the transaction resulted in a changed balance."""
         "*** YOUR CODE HERE ***"
+        if self.before != self.after:
+            return True
+        else:
+            return False
 
     def report(self):
         """Return a string describing the transaction.
@@ -21,7 +25,12 @@ class Transaction:
         msg = 'no change'
         if self.changed():
             "*** YOUR CODE HERE ***"
+            if self.before < self.after:
+                msg = f'increased {self.before}->{self.after}'
+            else:
+                msg = f'decreased {self.before}->{self.after}'
         return str(self.id) + ': ' + msg
+
 
 class BankAccount:
     """A bank account that tracks its transaction history.
@@ -65,6 +74,7 @@ class BankAccount:
     # *** YOU NEED TO MAKE CHANGES IN SEVERAL PLACES IN THIS CLASS ***
 
     def __init__(self, account_holder):
+        self.transactions = []
         self.balance = 0
         self.holder = account_holder
 
@@ -72,7 +82,10 @@ class BankAccount:
         """Increase the account balance by amount, add the deposit
         to the transaction history, and return the new balance.
         """
+        before = self.balance
         self.balance = self.balance + amount
+        temp = Transaction(len(self.transactions), before, self.balance)
+        self.transactions.append(temp)
         return self.balance
 
     def withdraw(self, amount):
@@ -80,8 +93,13 @@ class BankAccount:
         to the transaction history, and return the new balance.
         """
         if amount > self.balance:
+            temp = Transaction(len(self.transactions), self.balance, self.balance)
+            self.transactions.append(temp)
             return 'Insufficient funds'
+        before = self.balance
         self.balance = self.balance - amount
+        temp = Transaction(len(self.transactions), before, self.balance)
+        self.transactions.append(temp)
         return self.balance
 
 
@@ -108,14 +126,14 @@ class Server:
         """Append the email to the inbox of the client it is addressed to.
             email is an instance of the Email class.
         """
-        ____.inbox.append(email)
+        self.clients[email.recipient_name].inbox.append(email)
 
     def register_client(self, client):
         """Add a client to the clients mapping (which is a 
         dictionary from client names to client instances).
             client is an instance of the Client class.
         """
-        ____[____] = ____
+        self.clients[client.name] = client
 
 class Client:
     """A client has a server, a name (str), and an inbox (list).
@@ -138,11 +156,11 @@ class Client:
         self.inbox = []
         self.server = server
         self.name = name
-        server.register_client(____)
+        server.register_client(self)
 
     def compose(self, message, recipient_name):
         """Send an email with the given message to the recipient."""
-        email = Email(message, ____, ____)
+        email = Email(message, self, recipient_name)
         self.server.send(email)
 
 
@@ -182,9 +200,11 @@ class Mint:
 
     def create(self, coin):
         "*** YOUR CODE HERE ***"
+        return coin(self.year)
 
     def update(self):
         "*** YOUR CODE HERE ***"
+        self.year = self.present_year
 
 class Coin:
     cents = None # will be provided by subclasses, but not by Coin itself
@@ -194,6 +214,9 @@ class Coin:
 
     def worth(self):
         "*** YOUR CODE HERE ***"
+        if self.year + 50 >= Mint.present_year:
+            return self.cents
+        return self.cents + Mint.present_year - self.year - 50
 
 class Nickel(Coin):
     cents = 5
